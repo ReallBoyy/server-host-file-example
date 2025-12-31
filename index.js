18063452fa1205abc04aa99393e4cb6d01040a85
ReallBoyy <rahmanbariq@gmail.com>
2025-12-31 14:16:06 +0700

    Change server port to 8000 and update static file path
    Changed server port from 80 to 8000 and fixed static file serving path.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = 80;
+const PORT = 8000;
 
 // CDN static files
 app.set('trust proxy', 1);
@@ -16,7 +16,7 @@ app.use(function (req, res, next) {
     console.log(`[${new Date().toLocaleString()}] ${req.method} ${req.url} - ${res.statusCode}`);
     next();
 });
-app.use("/cdn", express.static(path.join(__dirname, "public"));
+app.use("/", express.static(path.join(__dirname, "public"));
 
 app.listen(PORT, () => {
   console.log(`CDN aktif di http://localhost:${PORT}`);
