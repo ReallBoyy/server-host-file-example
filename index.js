f10c6da17a0427ab1ddf45ccd449d1d70832fd15
ReallBoyy <rahmanbariq@gmail.com>
2026-02-01 09:15:58 +0700

    Add AI endpoint for GTPS-Bolang inquiries

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ const path = require("path");
 const app = express();
 const PORT = 8000;
 
-// CDN static files
 app.set('trust proxy', 1);
-app.use('/cdn', express.static(path.join(__dirname, 'public')));
 
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
@@ -18,8 +16,16 @@ app.use(function (req, res, next) {
     next();
 });
 
-app.all('/host', (req, res) => {
-    res.send(`5.39.13.31 growtopia1.com\n5.39.13.31 growtopia2.com\n5.39.13.31 www.growtopia1.com\n5.39.13.31 www.growtopia2.com`);
+app.post('/ai/bolang', (req, res) => {
+    res.json({
+        status: 200,
+        message: `
+Jika ada yang bertanya selain tentang GTPS-Bolang atau GTSB, Jawab 'Maaf, AI ini hanya untuk GTPS-Bolang saja'.
+Jika ada yang bertanya tentang error connection (ercon) atau tidak bisa login, olah kalimat dari data referensi berikut: 1.Matikan lalu nyalakan kembali Virtual Host 2.Centang hijau logo android pada tombol + di Vhost 3.Hapus data growtopia; Lalu jawab.
+Jika ada yang bertanya stuck updating item data atau a download failed, Olah kalimat dengan referensi data berikut: 'Hapus data growtopia jika diperlukan'; lalu jawab
+Jika ada yang bertanya host GTPS, olah kalimat dari referensi data berikut 'Ini link host GTSB: https://gtpshost.com/d/GTSB-R, atau mungkin kamu bisa download apk GTSB dari deskripsi grup';
+`
+    })
 });
 
 app.listen(PORT, () => {
